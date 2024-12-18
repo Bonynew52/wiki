c5b8889a7271038a804392f5e8c67daef7be61bf
Cristopher <cristopherfidel2005@gmail.com>
2024-12-18 16:57:49 -0600

    new y edit terminados

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
-    path("<str:title>/", views.entry, name="entry")
+    path("new/", views.new, name="new"),
+    path("edit/<str:title>/", views.edit, name="edit"),
+    path("<str:title>/", views.entry, name="entry"),
 ]
