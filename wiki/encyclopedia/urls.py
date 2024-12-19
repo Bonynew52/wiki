2628f9b831381e91096a28f2fe89973fcf71742c
Cristopher <cristopherfidel2005@gmail.com>
2024-12-19 11:29:29 -0600

    random page terminado

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
-    path("new/", views.new, name="new"),
-    path("edit/<str:title>/", views.edit, name="edit"),
-    path("<str:title>/", views.entry, name="entry"),
+    path("new_page/", views.new_page, name="new"),
+    path("random_page/", views.random_page, name="random"),
+    path("edit_page/<str:title>/", views.edit_page, name="edit"),
+    path("<str:title>/", views.entry, name="entry")
 ]
